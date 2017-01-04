e0a6eab58f4a246b30aecbef581517931808cda6
Lara-Anna Wagner <law94@gmx.at>
2017-01-04 14:45:20 +0100

    Creating project sections

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
-    
+    $(".portrait").mousemove(function(e){
+    	$('#thumbnail_hover').css("visibility","visible");
+  		$('#thumbnail_hover').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+	});
+	$(".portrait").mouseleave(function(e){
+    	$('#thumbnail_hover').css("visibility","hidden");
+  		
+	});
 });
+
+
+
