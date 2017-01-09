e96c4be41856db6b60656ec550db739a4b3359c0
Lara-Anna Wagner <law94@gmx.at>
2017-01-09 13:05:34 +0100

    adapting sass and thumbnais

--- script.js
+++ script.js
@@ -8,51 +8,93 @@ $( document ).ready(function() {
   		
 	});
 
-	$(".work").mousemove(function(e){
-    	$('#thumbnail_hover_2').css("visibility","visible");
-  		$('#thumbnail_hover_2').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+	$(".mediation").mousemove(function(e){
+    	$('#thumbnail_hover_1').css("visibility","visible");
+  		$('#thumbnail_hover_1').css({'top': e.clientY - 20, 'left': e.clientX - 20});
 	});
-	$(".work").mouseleave(function(e){
-    	$('#thumbnail_hover_2').css("visibility","hidden");
+	$(".mediation").mouseleave(function(e){
+    	$('#thumbnail_hover_1').css("visibility","hidden");
   		
 	});
 
 
+	$(".nagata").mousemove(function(e){
+    	$('#thumbnail_hover_2').css("visibility","visible");
+  		$('#thumbnail_hover_2').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+	});
+	$(".nagata").mouseleave(function(e){
+    	$('#thumbnail_hover_2').css("visibility","hidden");
+  		
+	});
 
+	$(".tommy").mousemove(function(e){
+    	$('#thumbnail_hover_5').css("visibility","visible");
+  		$('#thumbnail_hover_5').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+	});
+	$(".tommy").mouseleave(function(e){
+    	$('#thumbnail_hover_5').css("visibility","hidden");
+  		
+	});
+
+	$(".stubble").mousemove(function(e){
+    	$('#thumbnail_hover_3').css("visibility","visible");
+  		$('#thumbnail_hover_3').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+	});
+	$(".stubble").mouseleave(function(e){
+    	$('#thumbnail_hover_3').css("visibility","hidden");
+  		
+	});
+
+	$(".simon").mousemove(function(e){
+    	$('#thumbnail_hover_6').css("visibility","visible");
+  		$('#thumbnail_hover_6').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+	});
+	$(".simon").mouseleave(function(e){
+    	$('#thumbnail_hover_6').css("visibility","hidden");
+  		
+	});
+
+
+	$(".dover").mousemove(function(e){
+    	$('#thumbnail_hover_4').css("visibility","visible");
+  		$('#thumbnail_hover_4').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+	});
+	$(".dover").mouseleave(function(e){
+    	$('#thumbnail_hover_4').css("visibility","hidden");
+  		
+	});
 	
 
-    // Get the modal
-var modal = document.getElementById('modalMeditation');
-
-// Get the button that opens the modal
-var btn = document.getElementById("openMeditation");
+// Get the Meditation modal
+var modalMeditation = document.getElementById('modalMeditation');
+var buttonMeditation = document.getElementById("openMeditation");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeMeditation")[0];
+var closeMeditation = document.getElementsByClassName("closeMeditation")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+// open modal by click event
+buttonMeditation.onclick = function() {
+    modalMeditation.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+// close modal by click event
+closeMeditation.onclick = function() {
+    modalMeditation.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+// Get the Tommy modal
+var modalTommy = document.getElementById('modalTommy');
+var buttonTommy = document.getElementById("openTommy");
+
+var closeTommy = document.getElementsByClassName("closeTommy")[0];
+
+buttonTommy.onclick = function() {
+    modalTommy.style.display = "block";
 }
 
-
-
-
-
-
-
+closeTommy.onclick = function() {
+    modalTommy.style.display = "none";
+}
 
 });
 
